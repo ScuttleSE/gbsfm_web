41c6f2a4e480caf4c376067fad64fa8092f41e5b
Mads <mads@ab3.no>
2023-06-18 23:04:15 +0200

    Fix running gbs.fm with Django 4.2.2

--- playlist/forms.py
+++ playlist/forms.py
@@ -58,7 +58,7 @@ class SongIDField(forms.CharField):
   
 
 class UploadFileForm(forms.Form):
-  file  = forms.FileField(widget=forms.ClearableFileInput(attrs={'multiple': True}))
+  file  = forms.FileField(widget=forms.ClearableFileInput(attrs={'allow_multiple_selected': True}))
 
 class SearchForm(forms.Form):
   query = forms.CharField(max_length=100)
